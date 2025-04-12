595dd1974c326e41de183dac56752bd4d5b6a151
Jiyong Youn <01@0101010101.com>
2025-04-13 03:40:32 +0900

    update PWA icons

--- generate_icons.py
+++ generate_icons.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import cairosvg
-from PIL import Image
+from PIL import Image, ImageOps, ImageDraw
 
 sizes = {
     "regular": [16, 32, 180, 192, 512],
@@ -16,45 +16,81 @@ if not os.path.isfile('icons/skiing.svg'):
     exit(1)
 
 
+def create_gradient_background(width, height):
+    img = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+
+    for y in range(height):
+        r = int(18 + (y / height) * 20)
+        g = int(18 + (y / height) * 20)
+        b = int(18 + (y / height) * 20)
+        alpha = int(210 - (y / height) * 40)
+        draw.line([(0, y), (width, y)], fill=(r, g, b, alpha))
+
+    return img
+
+
 def create_regular_icon(size):
     output_file = f"icons/skiing-{size}x{size}.png"
 
+    padding_ratio = 0.1
+    icon_size = int(size * (1 - padding_ratio * 2))
+
     cairosvg.svg2png(
         url="icons/skiing.svg",
-        write_to=output_file,
-        output_width=size,
-        output_height=size,
-        background_color="#121212"
+        write_to="icons/temp_raw.png",
+        output_width=icon_size,
+        output_height=icon_size,
+        background_color="transparent"
     )
 
-    img = Image.open(output_file)
-    img.save(output_file)
+    icon = Image.open("icons/temp_raw.png")
+    icon = ImageOps.mirror(icon)
+
+    background = create_gradient_background(size, size)
+
+    padding = int(size * padding_ratio)
+    result = Image.new('RGBA', (size, size), (0, 0, 0, 0))
+    result.paste(background, (0, 0), background)
+    result.paste(icon, (padding, padding), icon)
+
+    result.save(output_file, format="PNG")
+    os.remove("icons/temp_raw.png")
     print(f"Created {output_file}")
 
 
 def create_maskable_icon(size):
     output_file = f"icons/skiing-maskable-{size}x{size}.png"
 
-    safe_zone_ratio = 0.8
-    icon_size = int(size * safe_zone_ratio)
-    padding = (size - icon_size) // 2
+    outer_padding_ratio = 0.1
+    inner_safe_zone_ratio = 0.8
 
-    img = Image.new('RGBA', (size, size), color="#121212")
+    actual_icon_size = int(size * inner_safe_zone_ratio *
+                          (1 - outer_padding_ratio * 2))
 
-    temp_file = "icons/temp_icon.png"
     cairosvg.svg2png(
         url="icons/skiing.svg",
-        write_to=temp_file,
-        output_width=icon_size,
-        output_height=icon_size,
+        write_to="icons/temp_raw.png",
+        output_width=actual_icon_size,
+        output_height=actual_icon_size,
         background_color="transparent"
     )
 
-    icon = Image.open(temp_file)
-    img.paste(icon, (padding, padding), icon)
-    img.save(output_file)
-    os.remove(temp_file)
+    icon = Image.open("icons/temp_raw.png")
+    icon = ImageOps.mirror(icon)
 
+    background = create_gradient_background(size, size)
+
+    inner_padding = int(size * (1 - inner_safe_zone_ratio) / 2)
+    outer_padding = int(size * outer_padding_ratio)
+    total_padding = inner_padding + outer_padding
+
+    result = Image.new('RGBA', (size, size), (0, 0, 0, 0))
+    result.paste(background, (0, 0), background)
+    result.paste(icon, (total_padding, total_padding), icon)
+
+    result.save(output_file, format="PNG")
+    os.remove("icons/temp_raw.png")
     print(f"Created {output_file}")
 
 
