2ee914d50a3eb5847cf53c24c12ef96561195dd9
Jiyong Youn <01@0101010101.com>
2025-04-13 03:35:14 +0900

    add a test server

---
+++ server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+
+PORT = 3001
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
